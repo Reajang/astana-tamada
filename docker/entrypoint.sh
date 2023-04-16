fbe1b80dabff2f4e55146c95f36d903880a55828
Леонид Гарин <garin@lanit.ru>
2023-04-16 13:18:09 +0200

    Правки в Dockerfile для ui

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -m
-echo resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) " ipv6=off;" > /etc/nginx/resolver.conf
+#echo resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) " ipv6=off;" > /etc/nginx/resolver.conf
 
-envsubst '\$BACKEND_UPSTREAM' < /etc/nginx/nginx.conf > /etc/nginx/conf.d/default.conf
+envsubst '\$BACKEND_UPSTREAM' < /etc/nginx/nginx.conf.tpl > /usr/local/openresty/nginx/conf/nginx.conf
 
 nginx -g 'daemon off;'
