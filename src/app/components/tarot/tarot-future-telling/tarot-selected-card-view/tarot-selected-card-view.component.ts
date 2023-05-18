12abb9a95df9cc6bfcf9da9e528837e2c1ee2d42
Леонид Гарин <garin@lanit.ru>
2023-05-18 13:29:53 +0200

    Refactoring

--- src/app/components/tarot/tarot-future-telling/tarot-selected-card-view/tarot-selected-card-view.component.ts
+++ src/app/components/tarot/tarot-future-telling/tarot-selected-card-view/tarot-selected-card-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TarotCard} from "../../../../models/tarot/tarot.model";
 
 @Component({
@@ -6,14 +6,9 @@ import {TarotCard} from "../../../../models/tarot/tarot.model";
   templateUrl: './tarot-selected-card-view.component.html',
   styleUrls: ['./tarot-selected-card-view.component.scss']
 })
-export class TarotSelectedCardViewComponent implements OnInit {
+export class TarotSelectedCardViewComponent {
 
   @Input() card?: TarotCard | undefined;
   @Output() onClick: EventEmitter<any> = new EventEmitter<any>();
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
